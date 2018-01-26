3d09953319fa802d7c371219532effa12f14af1a
ybdesire <ybdesire@gmail.com>
2018-01-26 20:34:34 +0800

    Init variable for java.

---
+++ ch1/InitVariable.java
@@ -0,0 +1,13 @@
+public class InitVariable
+{
+	public static void main(String []args)
+	{
+		int x = 4;
+		double y = 4.5;
+		boolean a = true;
+		boolean b = false;
+		System.out.println(a&&b);
+		System.out.println(a||b);
+		System.out.println(a && b);	
+	 }
+}
