1311bdb77dcbfda12c2a49dc7bda2a7c3c16b4ea
ybdesire <ybdesire@gmail.com>
2018-02-21 22:39:15 +0800

    Polymorphism

---
+++ ch3/Horse.java
@@ -0,0 +1,15 @@
+class Animal{
+   public void sound(){
+      System.out.println("Animal is making a sound");   
+   }
+}
+public class Horse extends Animal{
+public void sound(){
+        System.out.println("Neigh");
+    }
+    public static void main(String args[]){
+    	Animal obj = new Horse();
+    	obj.sound();
+    }
+}
+
